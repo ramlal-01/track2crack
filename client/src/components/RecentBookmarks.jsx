f880516ad1206f520c8da2e751b7cef9c4f76f9a
Riya Khandelwal <riyakhandelwal4776@gmail.com>
2025-11-24 00:28:06 +0530

    Bookmake make to remember

--- client/src/components/RecentBookmarks.jsx
+++ client/src/components/RecentBookmarks.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 
+import { useNavigate } from "react-router-dom";
 import API from "../api/api";
 const RecentBookmarks = () => {
   const [bookmarks, setBookmarks] = useState([]);
